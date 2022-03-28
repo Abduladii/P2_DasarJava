a7b51bd4cceed1ca19a99b0e473483923c179e36
user <abduladi0709@gmail.com>
2022-03-28 15:49:16 +0700

    java

--- src/_1_MengamatiHasilPerubahanNilaiDariSuatuOperasiMatematis.java
+++ src/_1_MengamatiHasilPerubahanNilaiDariSuatuOperasiMatematis.java
@@ -4,6 +4,7 @@ public class _1_MengamatiHasilPerubahanNilaiDariSuatuOperasiMatematis {
         float number2 = 6;
         float result = number1 / number2;
         float remainder = number1 % number2;
+
         System.out.println(number1 + "divided by" + number2);
         System.out.println("\nResult\tRemainder");
         System.out.println(result + "\t" + remainder);
